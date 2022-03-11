ca7dc23825dc6eafb2805992ee74f81b9877e54b
Rojina Amatya <rojinaamatya1432@gmail.com>
2022-03-11 13:45:13 -0600

    added the controller

--- SpringBoot_SpringMVC/src/main/java/com/example/controller/WelcomeController.java
+++ SpringBoot_SpringMVC/src/main/java/com/example/controller/WelcomeController.java
@@ -3,13 +3,14 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping("/welcome")
-
 public class WelcomeController {
 	
-	@RequestMapping("/index")
+	@RequestMapping("/first")
+	//@ResponseBody
 	public String index(Model model) {
 		//set data in Model to send to UI
 		
