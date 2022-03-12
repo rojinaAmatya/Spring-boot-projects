7fef9608bdf0014c3596737c3098010ee41883a0
Rojina Amatya <rojinaamatya1432@gmail.com>
2022-03-12 14:15:13 -0600

    creating new project

---
+++ SpringBoot_SpringMVC-01/src/main/java/com/demo/SpringBootSpringMvc01Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringMvc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringMvc01Application.class, args);
+	}
+
+}
