8fc8e95cd74e6a44a4bd59831dd68f3941c1e41e
Rojina Amatya <rojinaamatya1432@gmail.com>
2022-03-12 15:05:13 -0600

    adding controller

--- SpringBoot_SpringMVC-01/src/main/java/com/demo/controller/StudentController.java
+++ SpringBoot_SpringMVC-01/src/main/java/com/demo/controller/StudentController.java
@@ -1,6 +1,10 @@
 package com.demo.controller;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +14,17 @@ public class StudentController {
 	@RequestMapping(value ={"/", "/register"})
 	public String loadForm() {
 		return "register";
+	}
+	
+	public String handleStudentForm(HttpServletRequest req, Model m) {
+		
+		int sno = Integer.parseInt(req.getParameter("sno"));
+		String sname = req.getParameter("sname");
+		String sadd = req.getParameter("sadd");
+		m.addAttribute("sno", sno);
+		m.addAttribute("sname", sname);
+		m.addAttribute("sadd", sadd);
+		return "sucess";
 	}
 
 }
