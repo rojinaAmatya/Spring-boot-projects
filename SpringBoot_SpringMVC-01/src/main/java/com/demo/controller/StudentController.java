ba6fa6429ea97ba216d858c26aea748fd810ddf7
Rojina Amatya <rojinaamatya1432@gmail.com>
2022-03-13 13:45:13 -0500

    added post mapping

--- SpringBoot_SpringMVC-01/src/main/java/com/demo/controller/StudentController.java
+++ SpringBoot_SpringMVC-01/src/main/java/com/demo/controller/StudentController.java
@@ -5,6 +5,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,13 +15,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class StudentController {
 	
 	
-	@RequestMapping(value ={"/", "/login"})
+	//@RequestMapping(value ={"/", "/login"})
+	@GetMapping(value ={"/", "/login"})
 	public String loadForm() {
 		return "register";
 	}
 	
 	
-	@RequestMapping(value = "/register", method =RequestMethod.POST)
+	//@RequestMapping(value = "/register", method =RequestMethod.POST)
+	@PostMapping("/register")
 	public String handleStudentForm(HttpServletRequest req, Model m) {
 		
 		int sno = Integer.parseInt(req.getParameter("sno"));
