aab53410a2c17ab2d1893e66e63d6b52ce4ac304
Rojina Amatya <rojinaamatya1432@gmail.com>
2022-03-18 13:05:13 -0500

    modified for name

--- SpringBoot_Rest/src/main/java/com/demo/service/WelcomeController.java
+++ SpringBoot_Rest/src/main/java/com/demo/service/WelcomeController.java
@@ -3,6 +3,7 @@ package com.demo.service;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/welcome")
@@ -21,8 +22,8 @@ public class WelcomeController {
 	
 	
 	@GetMapping("/hello")
-	public String sayHello(String name) {
+	public String sayHello(@RequestParam("name") String name) {
 		return "Welcome: " + name;
 		}
-	
+
 }
