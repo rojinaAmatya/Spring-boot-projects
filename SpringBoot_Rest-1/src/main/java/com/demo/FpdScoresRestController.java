c3cacb493a35ccc0ffc12ce0626f9d605d12f30e
Rojina Amatya <rojinaamatya1432@gmail.com>
2022-03-18 13:25:13 -0500

    creating getmapping

--- SpringBoot_Rest-1/src/main/java/com/demo/FpdScoresRestController.java
+++ SpringBoot_Rest-1/src/main/java/com/demo/FpdScoresRestController.java
@@ -1,8 +1,18 @@
 package com.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FpdScoresRestController {
 
+	//http://localhost:8081/score?ssn=1545454254154
+	@GetMapping(value = "/score")
+	public String getFpdScore(@RequestParam("ssn") Long ssn) {
+		
+		//logic retrieve from db
+		return "FPD Score is : 75";
+	}
 }
