84b43a7f868c7270c34bce8efbd559308c66f187
Rojina Amatya <rojinaamatya1432@gmail.com>
2022-03-30 16:38:42 -0500

    added servlet initializer

---
+++ SpringBoot_SpringMVC-01/src/main/java/com/demo/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringBootSpringMvc01Application.class);
+	}	
+}
